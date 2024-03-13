04eb8765d67b9b08c37063082168fd614540c104
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-03-13 11:53:16 +0600

    Update index.jsx

--- src/components/predict/1win/lucky/index.jsx
+++ src/components/predict/1win/lucky/index.jsx
@@ -15,7 +15,7 @@ console.log(idss)
 
   const handleSubmits = async (e) => {
     e.preventDefault();
-    if(localStorage.getItem('id') === keytocheck+33){
+    if(localStorage.getItem('id') === keytocheck+5233){
       localStorage.setItem('token','active');
       navigate("/predict/lucky/active")
     }else{
